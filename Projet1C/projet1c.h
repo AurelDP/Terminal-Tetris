be9e75becd6036998b522e1cb1791aa3610c4b8b
cliele <clieledp@gmail.com>
2020-04-17 18:17:23 +0200

    [AJOUT] Prototype verif_validite

--- Projet1C/projet1c.h
+++ Projet1C/projet1c.h
@@ -25,6 +25,7 @@ void afficher_plateau(plateau jeu);
 
 void afficher_plateau(plateau jeu);
 void init_blocs(plateau * plat);
+int verif_validite(plateau jeu, int x, int y, int choix_bloc);
 void afficher_bloc(plateau plat, int * indices_blocs);
 
 void random_blocs(int * indices_blocs, plateau jeu);
