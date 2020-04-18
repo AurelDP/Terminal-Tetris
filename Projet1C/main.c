fb10ca5f4955a83c34f8da232c58655adb2d79c3
cliele <clieledp@gmail.com>
2020-04-18 19:36:17 +0200

    [AJOUTS & MODIFS] Regarder description
    - Ajout des tentatives maximum
    - Ajout des placements blocs
    - Ajout des tests lignes et colonnes
    - Ajout des annulations lignes et colonnes
    - Remplacement des textes et choix par des fonctions créées pour l'utilisation

--- Projet1C/main.c
+++ Projet1C/main.c
@@ -1,6 +1,4 @@
 #include "projet1c.h"
-// Information : Code ASCII du carré : 254
-// Code ASCII du point central : 250
 
 int main(){
     srand(time(NULL));
@@ -10,47 +8,24 @@ int main(){
     int indices_blocs[3] = {0}; // Dans le cas où la politique choisie est 2 (3 blocs aléatoires)
     int jouer = 1;
     int indice_choix, ligne, colonne;
+    int tentative = 1;
+    plateau jeu = {NULL, NULL, 0, 0, 0};
 
     // Affichage de l'accueil et sélection du choix du joueur
-    printf("#############################\n   Comme un air de Tetris\n#############################\n\n");
-    printf("1) Commencer a jouer\n2) Afficher les regles du jeu\nIndiquez votre choix : ");
-    scanf("%d",&choix);
-    while(choix != 1 && choix != 2){
-        printf("\nLe choix ne correspond pas aux propositions ...\nIndiquez votre choix : ");
-        scanf("%d",&choix);
-    }
+    choix = choix_debut();
 
     // Affichage des règles puis attente pour lancement du jeu
     if(choix == 2){
-        printf("\n\n");
-        printf("Le Tetris est un jeu qui se presente sous forme d'une matrice ou des blocs de differentes formes \n"
-               "doivent etre poses de sorte que le plateau soit garde le plus longtemps possible non plein. \n"
-               "L'idee est de placer chaque bloc a l'emplacement qui permet d'eliminer un maximum de lignes et/ou \n"
-               "de colonnes. Ces dernieres sont supprimees automatiquement lorsqu'elles sont pleines.\n"
-               "Ici, vous devrez placer les blocs qui vous seront fournis, dans un plateau d'une forme specifique.\n"
-               "Bonne chance !\n");
-        do{
-            printf("\nAppuyez sur '1' pour lancer le jeu : ");
-            scanf("%d",&choix);
-        }while(choix != 1);
+        choix = affichage_regles();
     }
 
     // Lancement du jeu
     if(choix == 1){
-        printf("\n\n");
-        plateau jeu = {NULL, NULL, 0, 0, 0};
 
-        // Choix de la taille du plateau
-        do{
-            printf("Taille de votre plateau (entre 21 et 25 compris, seulement en nombres impairs) : ");
-            scanf("%d", &jeu.taille);
-        }while(jeu.taille < 21 || jeu.taille > 25 || jeu.taille%2 == 0);
+        // Choix de la taille et de la forme du plateau
+        jeu = choix_plateau(jeu);
 
-        // Choix de la forme du plateau
-        do{
-            printf("\nForme de votre plateau \n1 : Cercle | 2 : Losange | 3 : Triangle \nVotre choix : ");
-            scanf("%d", &jeu.forme);
-        }while(jeu.forme != 1 && jeu.forme != 2 && jeu.forme != 3);
+        // Création du plateau en fonction de la forme choisie
         if(jeu.forme == 1){
             jeu = creer_plat_crc(jeu.taille, jeu.taille, jeu);
         } else if(jeu.forme == 2){
@@ -60,12 +35,9 @@ int main(){
         }
 
         // Choix de la politique de jeu (façon de jouer)
-        do{
-            printf("\nPolitique de jeu \n1 : Afficher a chaque tour de jeu l'ensemble des blocs disponibles et selection d'un seul a poser \n2 : Afficher uniquement 3 blocs selectionnes aleatoirement \nVotre choix : ");
-            scanf("%d", &jeu.politique);
-        }while(jeu.politique != 1 && jeu.politique != 2);
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        jeu = choix_politique(jeu);
 
+        // Initialisation du plateau accueillant les blocs du jeu en fonction de la forme choisie
         init_blocs(&jeu);
 
         // Début de la boucle de jeu
@@ -76,12 +48,35 @@ int main(){
             afficher_plateau(jeu);
             afficher_bloc(jeu, indices_blocs);
 
+            tentative = 1;
             do{
-                indice_choix = selection_bloc(indices_blocs, jeu);
+                indice_choix = selection_bloc(indices_blocs, jeu, tentative);
                 ligne = selection_coos_ligne(jeu);
                 colonne = selection_coos_colonne(jeu);
-            }while(!verif_validite(jeu, ligne, colonne, indice_choix, indices_blocs));
+                tentative ++;
+            }while(!verif_validite(jeu, ligne, colonne, indice_choix, indices_blocs) && tentative <= 3);
+
+            if(tentative == 4){
+                jouer = 0;
+            }
+
+            if(jouer){
+                jeu = placer_bloc(jeu, ligne, colonne, indice_choix, indices_blocs);
+                if(jeu.forme == 1 || jeu.forme == 2){
+                    for(int i = 0; i < jeu.taille; i++){
+                        if(etat_ligne(jeu, i)){
+                            jeu = annuler_ligne(jeu, i);
+                        }
+                    }
+                    for(int u = 0; u < jeu.taille; u++){
+                        if(etat_colonne(jeu, u)){
+                            jeu = annuler_colonne(jeu, u);
+                        }
+                    }
+                }
+            }
         }
+        message_fin(jeu);
     }
     return(0);
 }
